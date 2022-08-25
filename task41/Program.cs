490431ab25653916d05d2f2278a37b591e7a7f0d
Semilael <ariana-ses97@yandex.ru>
2022-08-25 12:10:34 +0300

    gitignore added

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] Array()
+{
+    int[] array = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine($"Введите {i+1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine($"Вы ввели числа: [{string.Join(", ", array)}].");
+    return array;
+}
+
+int Positive(int[] Array)
+{
+    int count = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (Array[i] >= 0) count++;
+    }
+    Console.WriteLine($"Положительных числел: {count}.");
+    return count;
+}
+
+Positive(Array());
